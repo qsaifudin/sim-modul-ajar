87657312469dee44a7e5b3bc09840ce71b710127
qsaifudin <duafinis@gmail.com>
2022-03-14 22:10:37 +0700

    tes

---
+++ zud/example_view.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<?php
+/*****************
+example code akses table oracle
+contoh table BARANG dengan kolom sbb :
+NOMOR NUMBER(10)
+NAMA_BARANG VARCHAR2(100)
+STOK NUMBER(2)
+//*****************/
+
+include "includes/func.inc.php";
+
+$db_user = "PA0010";
+$db_pass = "742309";
+$con = konekDb($db_user, $db_pass);
+
+echo "<strong>VIEW DATA BARANG</strong><br><br>";
+
+$nomor = 111;
+$sql = "SELECT * FROM PEGAWAI WHERE NOMOR=:v1";
+
+$data = array(
+    ':v1' => $nomor
+);
+
+$hasil = query_view($con, $sql,$data);
+oci_fetch_all($hasil, $rows, 0, 0, OCI_FETCHSTATEMENT_BY_ROW);
+
+    
+foreach ($rows as $hasil) {
+        echo "Nama Barang : " . $hasil['NIP'] ." - Stok : " . $hasil['NAMA'] . "<br>";
+    }
+?>
+</body>
+</html>
